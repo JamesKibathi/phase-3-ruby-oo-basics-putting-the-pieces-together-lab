52d2e872f6e525a6924a4c953f717ea18ffafa83
James Njenga <knjenga94@gmail.com>
2022-10-23 22:48:23 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+    attr_reader :title
+    def initialize(title)
+        @title=title
+    end
+    attr_accessor :author
+    attr_accessor :genre
+    attr_accessor :page_count
+    
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
+end
 
+book=Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
+book.turn_page
+
+#puts book.title
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_reader :brand
+    def initialize (brand)
+        @brand=brand
+    end
+    attr_accessor :material
+    attr_accessor :condition
+    attr_accessor :color
+    attr_accessor :size
+
+    def cobble
+        @condition="new"
+        puts ("Your shoe is as good as new!")
+    end
+end
+
+shoe=Shoe.new("Adidas")
+shoe=Shoe.new("Nike") 
+
